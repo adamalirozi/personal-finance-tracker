f50beee715c445aca13d1b18ba9083cc1a331479
Adam Ali Rozi <adam.ali@aims-cameroon.org>
2026-01-05 20:15:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -89,6 +89,6 @@ if __name__ == '__main__':
     print("Starting Personal Finance Tracker Backend")
     print("Backend running on http://127.0.0.1:5000")
     print("="*50 + "\n")
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
 
    # app.run(debug=True, port=5000, host='127.0.0.1')
